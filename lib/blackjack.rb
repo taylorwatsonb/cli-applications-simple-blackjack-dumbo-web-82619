864261d2ca683f70398194036b1aaa9d83289f40
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-23 19:12:34 +0000

    Automatically backed up by Learn

---
+++ lib/blackjack.rb
@@ -0,0 +1,94 @@
+    
+##################
+# shared methods #
+##################
+
+def welcome
+  # code #welcome here
+end
+
+def deal_card
+  rand(1..11)
+end
+
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+end
+
+
+##########
+# rounds #
+##########
+
+def welcome
+  puts "Welcome to the Blackjack Table"
+end
+
+
+def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
+end
+
+
+def initial_round
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
+end
+
+def get_user_input
+  gets.chomp.strip
+end
+
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+end
+
+
+
+def hit?(card_total)
+  valid_inputs = ["h", "s"]
+
+  prompt_user
+  user_input = get_user_input
+
+  until valid_inputs.include?(user_input)
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+
+  if user_input == "h"
+    card_total += deal_card
+  end
+  card_total
+  
+  ## Alternative solution - this solution calls hit? recursively if given an invalid command
+  # if user_input == "h"
+  #   card_total += deal_card
+  # elsif user_input == "s"
+  #   card_total
+  # else
+  #   invalid_command
+  #   hit?(card_total)
+  # end
+  
+end
+
+def invalid_command
+  puts "Please enter a valid command"
+end
+
+##########
+# runner #
+##########
+
+def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
+end
